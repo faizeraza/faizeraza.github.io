dba1bf821a80247527e9872459a49fcd6f11b53c
Faizan Raza <78217590+faizeraza@users.noreply.github.com>
2023-12-12 07:40:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 const shadows = document.querySelectorAll('.shadowz');
-  shadows.forEach(shadow => {
-    shadow.addEventListener('mousemove', (event) => {
-    const x = event.clientX - shadow.offsetLeft;
-    const y = event.clientY - shadow.offsetTop;
-
-    shadow.style.transform = `rotateX(${y / 50}deg) rotateY(${x / 50}deg)`;
-    shadow.style.boxShadow = `${x / 5}px ${y / 5}px 10px rgba(43, 56, 76, 0.09)`;
-  });
-
-  shadow.addEventListener('mouseout', () => {
-    shadow.style.transform = '';
-    shadow.style.boxShadow = '0px 24px 116px rgba(43, 56, 76, 0.09)'
-  });
-});const shadows = document.querySelectorAll('.shadowz');
 shadows.forEach(shadow => {
         shadow.addEventListener('mousemove', (event) => {
         const x = event.clientX - shadow.offsetLeft;
