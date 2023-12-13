be7e988dc95308e44e26a56ca7757277f021987c
Faizan Raza <78217590+faizeraza@users.noreply.github.com>
2023-12-13 18:42:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const shadows = document.querySelectorAll('.shadowz');
+const cursors = document.querySelectorAll('.cursor,.cursor1');
+let dimentions = [[50,50],[15,30],[10,10],[5,95],[0,65],[50,95],[90,90],[50,40],[30,15],[20,10],[95,20],[40,0],[95,95],[50,90]]
 shadows.forEach(shadow => {
         shadow.addEventListener('mousemove', (event) => {
         const x = event.clientX - shadow.offsetLeft;
@@ -12,10 +14,10 @@ shadows.forEach(shadow => {
         shadow.style.boxShadow = '0px 24px 116px rgba(43, 56, 76, 0.09)'
       });
 });
-const cursors = document.querySelectorAll('.cursor');
-let dimentions = [[50,50],[30,10],[10,30],[5,95]]
 document.addEventListener('mousemove', (e) => {
   for(var i=0;i<cursors.length;i++){
+    cursors[i].style.width = `5px`;
+    cursors[i].style.height = `5px`;
     cursors[i].style.top = `${e.pageY - 10}px`;
     cursors[i].style.left = `${e.pageX - 10}px`;
     cursors[i].style.display = 'block';
@@ -24,8 +26,11 @@ document.addEventListener('mousemove', (e) => {
 
 document.addEventListener('mouseout', () => {
   for(var i=0;i<cursors.length;i++){
+    cursors[i].style.width = `30px`;
+    cursors[i].style.height = `30px`;
     cursors[i].style.top = `${dimentions[i][0]}%`;
     cursors[i].style.left = `${dimentions[i][1]}%`;
     cursors[i].style.transform = 'rotate(360deg)';
   }
 });
+// the email operation
